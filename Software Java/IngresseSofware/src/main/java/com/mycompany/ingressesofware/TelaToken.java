48c8e56579404744d56a7e9437172de11a658d90
diegozn <diego.lsilva@bandtec.com.br>
2022-04-04 15:48:06 -0300

    visual TelaToken

--- Software Java/IngresseSofware/src/main/java/com/mycompany/ingressesofware/TelaToken.java
+++ Software Java/IngresseSofware/src/main/java/com/mycompany/ingressesofware/TelaToken.java
@@ -18,6 +18,7 @@ public class TelaToken extends javax.swing.JFrame {
      */
     public TelaToken() {
         initComponents();
+         setTitle("Confirmação Token");
     }
 
     /**
@@ -29,13 +30,34 @@ public class TelaToken extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnConfirmacaoToken = new javax.swing.JButton();
-        lblTokenSeguranca = new javax.swing.JLabel();
-        txtToken = new javax.swing.JTextField();
-        btnEnviarCodigo = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
         lblErro = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        btnConfirmacaoToken = new javax.swing.JButton();
+        txtToken = new javax.swing.JTextField();
+        lblTokenSeguranca = new javax.swing.JLabel();
+        btnEnviarCodigo = new javax.swing.JButton();
+        lblErro1 = new javax.swing.JLabel();
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("asd");
+        setBackground(new java.awt.Color(97, 0, 131));
+        setFocusable(false);
+
+        lblErro.setForeground(new java.awt.Color(255, 0, 0));
+
+        jPanel1.setBackground(new java.awt.Color(97, 0, 131));
 
         btnConfirmacaoToken.setText("OK");
         btnConfirmacaoToken.addActionListener(new java.awt.event.ActionListener() {
@@ -44,13 +66,15 @@ public class TelaToken extends javax.swing.JFrame {
             }
         });
 
-        lblTokenSeguranca.setText("Digite o Token de segurança");
-
         txtToken.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtTokenActionPerformed(evt);
             }
         });
+
+        lblTokenSeguranca.setFont(new java.awt.Font("Liberation Sans", 1, 15)); // NOI18N
+        lblTokenSeguranca.setForeground(new java.awt.Color(255, 255, 255));
+        lblTokenSeguranca.setText("Digite o Token de segurança");
 
         btnEnviarCodigo.setText("Enviar código");
         btnEnviarCodigo.addActionListener(new java.awt.event.ActionListener() {
@@ -59,7 +83,46 @@ public class TelaToken extends javax.swing.JFrame {
             }
         });
 
-        lblErro.setForeground(new java.awt.Color(255, 0, 0));
+        lblErro1.setFont(new java.awt.Font("Liberation Sans", 1, 14)); // NOI18N
+        lblErro1.setForeground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(122, 122, 122)
+                        .addComponent(lblErro1, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGap(104, 104, 104)
+                        .addComponent(lblTokenSeguranca, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(111, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnConfirmacaoToken, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(btnEnviarCodigo, javax.swing.GroupLayout.DEFAULT_SIZE, 213, Short.MAX_VALUE)
+                        .addComponent(txtToken)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(btnEnviarCodigo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblTokenSeguranca)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtToken, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnConfirmacaoToken)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblErro1, javax.swing.GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE)
+                .addGap(28, 28, 28))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -67,31 +130,25 @@ public class TelaToken extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(105, 105, 105)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(lblTokenSeguranca, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(txtToken)
-                    .addComponent(btnConfirmacaoToken, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btnEnviarCodigo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(lblErro, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(118, Short.MAX_VALUE))
+                .addComponent(lblErro, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(17, 17, 17)
-                .addComponent(btnEnviarCodigo)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(lblTokenSeguranca)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtToken, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnConfirmacaoToken)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lblErro)
-                .addContainerGap(36, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        pack();
+        getAccessibleContext().setAccessibleName("asd");
+        getAccessibleContext().setAccessibleDescription("asd");
+        getAccessibleContext().setAccessibleParent(lblTokenSeguranca);
+
+        setSize(new java.awt.Dimension(429, 226));
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     Integer randomToken = ThreadLocalRandom.current().nextInt(5023, 10023);
@@ -105,7 +162,7 @@ public class TelaToken extends javax.swing.JFrame {
                 dispose();
                 
             } else {
-              lblErro.setText("O token está incorreto");
+              lblErro1.setText("O token está incorreto");
             }
     }//GEN-LAST:event_btnConfirmacaoTokenActionPerformed
 
@@ -116,6 +173,7 @@ public class TelaToken extends javax.swing.JFrame {
     private void btnEnviarCodigoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEnviarCodigoActionPerformed
         SlackIntegrationTest slackAlert = new SlackIntegrationTest();
         slackAlert.sendMessageToSlack(randomToken.toString());
+        lblErro1.setText("O Código foi enviado");
     }//GEN-LAST:event_btnEnviarCodigoActionPerformed
 
     /**
@@ -157,7 +215,10 @@ public class TelaToken extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnConfirmacaoToken;
     private javax.swing.JButton btnEnviarCodigo;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JLabel lblErro;
+    private javax.swing.JLabel lblErro1;
     private javax.swing.JLabel lblTokenSeguranca;
     private javax.swing.JTextField txtToken;
     // End of variables declaration//GEN-END:variables
