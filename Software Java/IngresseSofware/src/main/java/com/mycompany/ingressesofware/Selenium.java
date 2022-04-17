36adc509fbcd8d4aa84ac6cea081799afeec6749
diegozn <diego.lsilva@bandtec.com.br>
2022-04-17 20:53:27 -0300

    resolucao telaLogin + automacao pt1

---
+++ Software Java/IngresseSofware/src/main/java/com/mycompany/ingressesofware/Selenium.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ingressesofware;
+
+import com.sun.tools.javac.Main;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import org.jsoup.Jsoup;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ *
+ * @author diego.silva@VALEMOBI.CORP
+ */
+public class Selenium {
+
+    public static void main(String[] args) throws IOException {
+
+  
+}
